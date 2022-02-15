16b55434b097eacf0bda78bf9839c0f94a11d75a
Tama McGlinn <t.mcglinn@gmail.com>
2022-02-15 12:39:09 +0100

    use adabots 1.2.0 simplified interface in build_wall

--- src/build_wall.adb
+++ src/build_wall.adb
@@ -1,27 +1,27 @@
-with Adabots;
+with Adabots.Single_Turtle;
 
 procedure Build_Wall is
-   Robot : constant Adabots.Turtle := Adabots.Create_Turtle;
+   package Bot renames Adabots.Single_Turtle;
 begin
 
    --  Go to floor
    loop
-      exit when Robot.Down = False;
+      exit when Bot.Down = False;
    end loop;
 
    --  One back up
-   Robot.Up;
+   Bot.Up;
 
    --  Turn around
-   Robot.Turn_Left;
-   Robot.Turn_Left;
+   Bot.Turn_Left;
+   Bot.Turn_Left;
 
    --  Build wall backwards
    for Wall_Index in 1 .. 3 loop
-      Robot.Place_Down;
-      Robot.Place_Up;
-      Robot.Back;
-      Robot.Place;
+      Bot.Place_Down;
+      Bot.Place_Up;
+      Bot.Back;
+      Bot.Place;
    end loop;
 
 end Build_Wall;
